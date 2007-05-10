3138b7dff1e0d3b00c20a8c57aa7ce684e9b5573
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-05-10 22:46:11 +0000

    * unit test framework
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/software/recorder/trunk@2098 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

---
+++ unittest/H5FileRecorder_create.cc
@@ -0,0 +1,39 @@
+
+#include <boost/test/auto_unit_test.hpp>
+
+#include "boost/filesystem/operations.hpp" // includes boost/filesystem/path.hpp
+#include "boost/filesystem/fstream.hpp"    // ditto
+#include <iostream>                        // for std::cout
+#include "h5filerecorder.h"
+using namespace soma; 
+using  namespace boost;       
+using namespace boost::filesystem; 
+
+BOOST_AUTO_TEST_SUITE(H5FileRecorder); 
+
+BOOST_AUTO_TEST_CASE(H5FileRecorder_test )
+{
+  
+  std::string filename = "test.h5";
+  filesystem::remove_all(filename); 
+  recorder::H5FileRecorder * prec = new recorder::H5FileRecorder(filename); 
+
+  prec->createEpoch("test1"); 
+  prec->switchEpoch("test1"); 
+
+  prec->enableRX(TSPIKE, 10); 
+
+
+
+  delete prec; 
+
+
+  
+  // confirm existence of file
+  path p(filename); 
+  BOOST_CHECK( exists(p) );
+
+  
+}
+
+BOOST_AUTO_TEST_SUITE_END(); 
